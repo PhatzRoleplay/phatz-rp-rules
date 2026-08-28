156ca7a35f68b80fdb67de1fcfd8867f7c95b72a
Phatz RP <teatiawa123@gmail.com>
2026-08-28 23:12:48 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+const themeToggle =
+  document.getElementById("themeToggle");
 
+
+const savedTheme =
+  localStorage.getItem("phatz-theme");
+
+
+if (savedTheme === "light") {
+
+  document.body.classList.add("light-mode");
+
+  themeToggle.textContent =
+    "Dark Mode";
+
+}
+
+
+themeToggle.addEventListener(
+  "click",
+  function () {
+
+    document.body.classList.toggle(
+      "light-mode"
+    );
+
+
+    if (
+      document.body.classList.contains(
+        "light-mode"
+      )
+    ) {
+
+      localStorage.setItem(
+        "phatz-theme",
+        "light"
+      );
+
+      themeToggle.textContent =
+        "Dark Mode";
+
+    }
+
+    else {
+
+      localStorage.setItem(
+        "phatz-theme",
+        "dark"
+      );
+
+      themeToggle.textContent =
+        "Light Mode";
+
+    }
+
+  }
+);
